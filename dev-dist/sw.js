34bef5007da12d987dac27d7804ba10da1b654fa
sateesh Mandanapu <guoa@txu.com>
2026-05-29 10:49:39 -0500

    Replace remaining generic emojis with the new official premium logo across all pages

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.26bc8ct1f8g"
+    "revision": "0.c2gpt9f4v1g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
